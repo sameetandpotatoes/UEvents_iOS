a8f838d3a14aaa72ec7473fb49045429a39967db
Sameet Sapra <sameetandpotatoes@gmail.com>
2014-06-29 13:14:15 -0500

    Initial Commit

---
+++ UEvents/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UEvents
+//
+//  Created by Sameet Sapra on 6/29/14.
+//  Copyright (c) 2014 Sameet Sapra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
